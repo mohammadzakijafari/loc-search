36c1f9865b6df14573409656f83978824d3b9138
mohammadzakijafari <zaki.jafari5@gmail.com>
2025-12-29 23:49:32 +0100

    navigation with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react"
+import type { Place } from "./api/Place"
+import LocationSearch from "./components/LocationSearch";
+import Map from "./components/Map";
 
 function App() {
 
+  const [place, setPlace] = useState<null | Place>(null);
+
   return (
-    <div className="m-10">
-      Hi There!!
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3">
+        <LocationSearch onPlaceClick={(p) => setPlace(p)} />
+      </div>
+      <div className="col-span-9">
+        <Map place={place} />
+      </div>
     </div>
   )
 }
---
+++ src/api/Place.ts
@@ -0,0 +1,6 @@
+export interface Place {
+    id: number,
+    name: string,
+    langitude: number,
+    latitude: number
+}
